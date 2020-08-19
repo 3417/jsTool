70ea3e10d2656590faea2b5037dafa4ca8ee8761
3417 <51107990+3417@users.noreply.github.com>
2020-08-19 16:49:12 +0800

    Update t1.js

--- t1.js
+++ t1.js
@@ -1,4 +1,4 @@
-// 下划线转换为驼峰
+// 1.下划线转换为驼峰
 function toHump(name) {
     return name.replace(/\_(\w)/g, function (all, letter) {
         return letter.toUpperCase();
@@ -6,12 +6,12 @@ function toHump(name) {
 }
 
 
-// 驼峰转换为下划线
+// 2.驼峰转换为下划线
 function toLine(name) {
     return name.replace(/([A-Z])/g, '_$1').toLowerCase();
 }
 
-// axios下载流文件
+// 3.axios下载流文件
 axios({
     method: "get/post",
     url: "xxx",
@@ -35,7 +35,7 @@ axios({
 
 
 
-// 格式化对象
+// 4.格式化对象
 function filterObj(obj) {
     let new_obj = {}
     if(obj === "" || obj === null || obj === undefined) return new_obj;
@@ -59,3 +59,33 @@ let obj = {
     e: null
 }
 console.log('最终结果',filterObj(obj))
+
+
+// 5.es6去重简单的数据
+let templ = [...new Set([1, 1, 2, 2, 3, 3])]
+let templ2 = Array.from(new Set([1, 1, 2, 2, 3, 3]))
+
+// 6.二维数组简化为一维数组
+let flats = [
+    [1, 2],
+    [3, 4]
+].flat();
+
+// 7.根据数字生成对应的数组
+let arr1 = Array.from({
+    length: 30
+}, (v, k) => k + 1)
+let arr2 = Array.from(Array(30), (v, k) => k + 1)
+let arr3 = new Array(30).keys()
+Array.from(arr3, item => item + 1)
+
+// 8.快速生成英文26字母
+//upper
+export const upperLetter = Array.from({
+    length: 26
+}, (_, index) => String.fromCodePoint(65 + index))
+//lower
+export const lowerletter = Array.from({
+        length: 26
+    }, (_, index) =>
+    String.fromCodePoint(97) + index)
