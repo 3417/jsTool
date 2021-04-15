fd01c53b90c51de9545988b896d259ca671dad9c
liangdong <1789719643@qq.com>
2021-04-15 09:53:36 +0800

    feat: 添加新的模块

--- t4.js
+++ t4.js
@@ -59,3 +59,22 @@ Vue.use(hasPermission);
 
 // xxxx.vue
 <el-button v-has="car:info:add"></el-button>
+
+
+/**
+ * WebSocket 完w3school建立一个IO链接
+ * 
+ * */ 
+
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:8080');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
